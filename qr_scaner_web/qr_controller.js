5096971e1bd24ddad66b2fb30dfe568f2d9097d7
LSuevalov <82171050+LSuevalov@users.noreply.github.com>
2023-08-29 06:55:42 +0300

    Add files via upload

--- qr_scaner_web/qr_controller.js
+++ qr_scaner_web/qr_controller.js
@@ -15,7 +15,7 @@
 
 import QrScanner from "./qr-scanner.min.js";
 
-const tableUrl = "https://script.google.com/macros/s/AKfycbzKkyQY4hf4-6iXEPvjKYAinaS-TZqp3ptu8WipDn4Qj5uEaAtsMnabM20EVW7fi3qLZQ/exec";
+const tableUrl = "https://script.google.com/macros/s/AKfycby6S9tGpGPa7vmbn9h41AYIdtp6wO31nqwMJZUuwuNkMy7AxDsTSItFeRkSeLuY6OS0NQ/exec";
 const scanner = new QrScanner(document.getElementById('qr-video'), result => scanCallback(result), {
     // onDecodeError: error => {
     //     alert(`Ошибка распознавания: ${error}`);
@@ -24,10 +24,11 @@ const scanner = new QrScanner(document.getElementById('qr-video'), result => sca
     highlightCodeOutline: true,
 });
 
-let sectorNumber, code;
+let sectorNumber, code, scannerIsInitialized;
 
 let sectorSelectionModal, sectorNumberInput, sectorNumberSubmitButton, sectorButton;
-let video, confirmationZone, scanResultText, sendResultsButton;
+let settingsZone, noCamAccessZone, camList, useFlashToggle;
+let video, confirmationZone, scanResultText, sendResultsButton, cancelResultsButton, requestResultDisplay;
 
 $(document).ready(function () {
     // alert('hi!');
@@ -36,26 +37,126 @@ $(document).ready(function () {
     sectorNumberSubmitButton = $("#sectorNumberSubmitButton");
     sectorButton = $("#sectorButton");
 
+    sectorButton.click(showSectorSelectionModal);
+
+
+    settingsZone = $("#settingsZone");
+    noCamAccessZone = $("#noCamAccessZone");
+    camList = $("#camList");
+    // useFlashToggle = $("#useFlashToggle");
+
+    noCamAccessZone.show();
+    settingsZone.hide();
+
+
     video = $("#video");
     confirmationZone = $("#confirmationZone");
     scanResultText = $("#scanResult");
     sendResultsButton = $("#sendResultsButton");
+    cancelResultsButton = $("#cancelResultsButton");
+    requestResultDisplay = $("#requestResultDisplay");
 
-    sectorSelectionModal.modal('toggle');
-    sectorNumberSubmitButton.click(setSectorNumber);
+    sendResultsButton.click(sendScanResults);
+    cancelResultsButton.click(cancelScanResults);
     video.show();
     confirmationZone.hide();
 
-    sendResultsButton.click(sendScanResults);
+
+    $( "#sectorNumberForm" ).on("submit", function( event ) {
+        event.preventDefault();
+        submitSectorForm();
+    });
+
+    $('#settingsModal').on('shown.bs.modal',  function (e) {
+        $('#settingsModal').find('[data-toggle="toggle"]').bootstrapToggle('destroy');
+        $('#settingsModal').find('[data-toggle="toggle"]').bootstrapToggle();
+    });
+
+    camList.on('change', function(event) {
+        switchCamera();
+    });
+
+    // useFlashToggle.on('click', function(event) {
+    //     if(useFlashToggle.prop('checked')) {
+    //         scanner.turnFlashOff().then(() => useFlashToggle.prop('checked', scanner.isFlashOn()));
+    //     } else {
+    //         scanner.turnFlashOn().then(() => useFlashToggle.prop('checked', scanner.isFlashOn()));
+    //     }
+    // });
+
+    showSectorSelectionModal();
 });
+
+
+//region Sector Selection
+function showSectorSelectionModal() {
+    console.log('show!')
+    if (sectorNumber === undefined || typeof sectorNumber !== "number") {
+        sectorSelectionModal.find('button.close').hide();
+
+        sectorSelectionModal.removeData('bs.modal').modal({
+            backdrop: 'static',
+            keyboard: false
+        }, 'show');
+    } else {
+        sectorNumberInput.val(sectorNumber);
+        sectorSelectionModal.find('button.close').show();
+
+        sectorSelectionModal.removeData('bs.modal').modal({
+            backdrop: true,
+            keyboard: true
+        }, 'show');
+    }
+}
+
+function submitSectorForm() {
+    setSectorNumber();
+    sectorSelectionModal.modal('toggle');
+    if(!scannerIsInitialized) {
+        initializeScanner();
+    }
+}
 
 function setSectorNumber() {
     sectorNumber = parseInt(sectorNumberInput.val());
     sectorButton.text(`#${sectorNumber}`);
+}
+// endregion
 
-    scanner.start();
+//region Scanner Settings
+
+function switchCamera() {
+    scanner.setCamera(camList.val()).then(
+        // updateFlashAvailability
+    );
 }
 
+// function updateFlashAvailability() {
+//     scanner.hasFlash().then(hasFlash => {
+//         useFlashToggle.prop("disabled", !hasFlash);
+//     });
+// }
+
+function initializeScanner() {
+    QrScanner.listCameras(true).then(
+        function(cameras) {
+            cameras.sort((a, b) => a.id - b.id).forEach(camera => {
+                camList.append($('<option>', {
+                    value: camera.id,
+                    text: camera.label
+                }));
+            });
+
+            switchCamera();
+            scanner.start().then(function() {
+                noCamAccessZone.show();
+                settingsZone.hide();
+            });
+        });
+}
+//endregion
+
+//region Scan Result Handling
 function scanCallback(result) {
     if(!validateScanResult(result.data)) {
         alert(`Невалидный результат ${result.data}`);
@@ -67,7 +168,9 @@ function scanCallback(result) {
 
 function validateScanResult(resultData) {
     // TODO: validation
-    return true;
+    let split = resultData.split("-");
+    return (split.length === 2 && !isNaN(parseInt(split[0])))
+    // return true;
 }
 
 function handleScanResult(resultData) {
@@ -81,72 +184,60 @@ function handleScanResult(resultData) {
 }
 
 function sendScanResults() {
-    $.post(tableUrl,
-        {
-            code: code,
-            sector: sectorNumber
-        });
+    let params = `?code=${code}&sector=${sectorNumber}`;
+    $.get(tableUrl+params, createRequestCallback(code));
+    // $.post(tableUrl,
+    //     {
+    //         code: code,
+    //         sector: sectorNumber
+    //     });
 
+    cancelScanResults();
+}
+
+function cancelScanResults() {
     confirmationZone.hide();
     video.show();
     scanner.start();
+    code = undefined;
 }
 
-function showRequestResults() {
-
+function createRequestCallback(code) {
+    return function (data, status) {
+        // console.log(`code ${code} result: ${data} ${status}`);
+        showRequestResults(code, data, status);
+    };
 }
 
-//
-// function setResult(label, result) {
-//     console.log(result.data);
-//     label.textContent = result.data;
-//     camQrResultTimestamp.textContent = new Date().toString();
-//     label.style.color = 'teal';
-//     clearTimeout(label.highlightTimeout);
-//     label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
-// }
-//
+function showRequestResults(code, data, status) {
+    let isSuccess = status === 'success';
+    let message;
+    if(isSuccess) {
+        requestResultDisplay.addClass('alert-success');
+        requestResultDisplay.removeClass('alert-danger');
+        message = `<i class="bi bi-check"></i> ${code} записан`;
+    } else {
+        requestResultDisplay.addClass('alert-danger');
+        requestResultDisplay.removeClass('alert-success');
+        message = `<i class="bi bi-x"></i> ${code} не записан: ${data}`;
+    }
+
+    requestResultDisplay.html(message);
+    requestResultDisplay.show();
+    requestResultDisplay.stop();
+    requestResultDisplay.fadeOut(5000);
+}
+
+//endregion
+
 // // ####### Web Cam Scanning #######
-//
 
-//
-// const updateFlashAvailability = () => {
-//     scanner.hasFlash().then(hasFlash => {
-//         camHasFlash.textContent = hasFlash;
-//         flashToggle.style.display = hasFlash ? 'inline-block' : 'none';
-//     });
-// };
-//
-// scanner.start().then(() => {
-//     updateFlashAvailability();
-//     // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
-//     // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
-//     // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
-//     // start the scanner earlier.
-//     QrScanner.listCameras(true).then(cameras => cameras.forEach(camera => {
-//         const option = document.createElement('option');
-//         option.value = camera.id;
-//         option.text = camera.label;
-//         camList.add(option);
-//     }));
-// });
-//
-// QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
-//
 // // for debugging
 // window.scanner = scanner;
+
 //
-// document.getElementById('inversion-mode-select').addEventListener('change', event => {
-//     scanner.setInversionMode(event.target.value);
-// });
+
 //
-// camList.addEventListener('change', event => {
-//     scanner.setCamera(event.target.value).then(updateFlashAvailability);
-// });
-//
-// flashToggle.addEventListener('click', () => {
-//     scanner.toggleFlash().then(() => flashState.textContent = scanner.isFlashOn() ? 'on' : 'off');
-// });
 //
 // document.getElementById('start-button').addEventListener('click', () => {
 //     scanner.start();
